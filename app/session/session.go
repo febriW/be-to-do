9f6131d0538cefc3871dca6ffbfa9e4a22266ce7
febriw <febriwanda26@gmail.com>
2024-11-05 15:13:40 +0700

    added middleware

---
+++ app/session/session.go
@@ -0,0 +1,43 @@
+package session
+
+import (
+	"math/rand/v2"
+	"strings"
+	"sync"
+)
+
+var (
+	m            sync.Mutex
+	sessionStore = make(map[string]int)
+)
+
+func Create(id int) string {
+	m.Lock()
+	defer m.Unlock()
+
+	key := generate(8)
+	if _, ok := sessionStore[key]; !ok {
+		sessionStore[key] = id
+	}
+	return key
+}
+
+func Get(token string) (int, bool) {
+	m.Lock()
+	defer m.Unlock()
+
+	v, ok := sessionStore[token]
+	return v, ok
+}
+
+func generate(n int) string {
+	chars := "0123456789abcdefghijklmnopqrstuvwxyz"
+	var sb strings.Builder
+	sb.Grow(n)
+
+	for i := 0; i < n; i++ {
+		idx := rand.IntN(len(chars))
+		sb.WriteByte(chars[idx])
+	}
+	return sb.String()
+}

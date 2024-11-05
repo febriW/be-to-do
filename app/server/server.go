b51630d19949f8415675fec6f78516afdb75c369
febriw <febriwanda26@gmail.com>
2024-11-05 09:31:49 +0700

    added gitignore, added server

---
+++ app/server/server.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func New(mux *http.ServeMux, addr string) *http.Server {
+	return &http.Server{
+		Handler:           mux,
+		Addr:              addr,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+	}
+}
+
+func ErrorResponse(w http.ResponseWriter, code int, err error) {
+	slog.Error("Error response", "err", err, "code", code)
+	w.WriteHeader(code)
+	output := struc {
+		Error string `json:"error"`
+	}{
+		Error: err.Error(),
+	}
+
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		slog.Error("Failed writing error response", "err", err)
+	}
+}
+
+func JSONResponse(w http.ResponseWriter, code int, data any) {
+	w.WriteHeader(code)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		slog.Error("Failed writing error response", "err", err, "data", data)
+	}
+}
